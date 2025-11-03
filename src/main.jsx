061151aadca391b0d54c9993c5cafd716a1ce3f3
Mudavathv123 <mudavathvinod49@gmail.com>
2025-11-03 14:56:56 +0530

    css link added in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './style.css'
+import './../public/css/styles.css';
+import './../public/css/responsiveness.css';
+import './../public/css/team_animations.css';
+import './../public/css/footer_professional.css';
+import './../public/css/mobileview_styles.css';
+import './../public/css/components.css';
+
 
 const container = document.getElementById('app')
 createRoot(container).render(
