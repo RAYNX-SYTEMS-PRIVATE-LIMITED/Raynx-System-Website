a5aaef1c66090f23d813d6f99e29af62a2b28e64
Mudavathv123 <123620615+Mudavathv123@users.noreply.github.com>
2026-01-23 15:01:30 +0530

    Make Google Maps iframe responsive
    Updated the Google Maps iframe to a responsive design with adjusted dimensions.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -265,7 +265,16 @@ export default function Footer() {
           >
             Our Location
           </h4>
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.249078858112!2d78.553112324935!3d17.399830283489493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9941a0ba5da7%3A0xf60017a02a426bc9!2sDSL%20Abacus%20IT%20Park!5e0!3m2!1sen!2sin!4v1769160163541!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.249078858112!2d78.553112324935!3d17.399830283489493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9941a0ba5da7%3A0xf60017a02a426bc9!2sDSL%20Abacus%20IT%20Park!5e0!3m2!1sen!2sin!4v1769160163541!5m2!1sen!2sin"
+  width="100%"
+  height="250"
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+/>
+
         </div>
       </div>
 
